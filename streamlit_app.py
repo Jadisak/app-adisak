b6a7ca9e6f731395beb16674232459def6f20b5b
Adisak Jantra Sopark <ajsopark@gmail.com>
2024-03-12 23:43:38 +0700

    003

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,26 @@
 import streamlit as st
 
+
+st.sidebar.title('Input')
+url_input = st.sidebar.text_input('YOUR TEXT', '')
+
+if url_input:
+    st.subheader('Output')
+    st.success(f'The wind of happy are gone : {url_input}')
+else:
+    st.subheader('Enter your URL please')
+    st.error('Awaiting your INPUT...!')
+
 st.title('🎈 ST-App')
 
-st.title("This is a Custom-CSS")
+st.header("This is a Custom-CSS")
 
 col1, col2, col3 =st.columns(3)
+
 col1.metric("Temperature", "70 °F", "1.2 °F")
-col1.metric("Temperature", "78 °F", "1.4 °F")
 col2.metric("Wind", "9 mph", "-8%")
-col2.metric("Wind", "8.32 mph", "-7.9%")
 col3.metric("Humidity", "86%", "1.2 °F")
+
+col1.metric("Temperature", "78 °F", "1.4 °F")
+col2.metric("Wind", "8.32 mph", "-7.9%")
 col3.metric("Humidity", "83%", "1.2 °F")
